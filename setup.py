f257ea7251859f2ffdf8af98b2bb01ea19efd882
PaulDanPhillips <paul.dan.phillips@gmail.com>
2024-05-15 12:54:00 -0700

    Added license and got Snakefile into build from setup.py

--- setup.py
+++ setup.py
@@ -7,14 +7,16 @@ setup(
     author="Paul Phillips",
     description="A machine learning approach for conducting genome wide association studies (GWAS) on bacteria",
     name="bacGWAStatLearn",
-    packages=find_packages(include=["bacGWAStatLearn", "bacGWAStatLearn.*"]),
-    version="0.1.1",
+    packages=find_packages(include=["bacGWAStatLearn", "bacGWAStatLearn.*", "bacGWAStatLearn.Snakefile"]),
+    version="0.1.3",
+    package_data={'bacGWAStatLearn': ['Snakefile']},
     install_requires=['pandas', 'scipy', 'statsmodels','numpy==1.23.5', 'scikit-learn', 'xgboost', 'matplotlib', 'joblib', 'snakemake', 'Boruta', 'shap', 'seaborn','plotnine'],
-    # packages=["bacGWASim"],
+    # py_modules=['Snakefile', 'ClassIndexing.py'],
+    url="https://github.com/PaulDanPhillips/bacGWAStatLearn",
+    license="MIT",
     entry_points="""
     [console_scripts]
     {program} = bacGWAStatLearn.main:main
     """.format(program=_program),
     include_package_data=True,
 )
-
